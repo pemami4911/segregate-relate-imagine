9f77dd361a56fa93347efb5e0f235a757e74c48e
Patrick Emami <pemami@nrel.gov>
2023-09-15 17:16:45 -0500

    updates for eval

--- compute_fid.py
+++ compute_fid.py
@@ -14,7 +14,7 @@
 import os
 from os import path as osp
 from tqdm import tqdm
-import pathlib
+from pathlib import Path
 import shutil
 
 import torch
@@ -23,29 +23,39 @@ from PIL import Image
 
 from third_party.genesis_v2.geco import GECO
 from sri.model import SRI
+from sri.fixed_order_model import FixedOrderSRI
 from third_party.pytorch_fid import fid_score as FID
 from third_party.genesis_v2.shapestacks_config import ShapeStacksDataset
 import argparse
 
-local_rank = os.environ['LOCAL_RANK']
 torch.set_printoptions(threshold=10000, linewidth=300)
 
 
-def restore_from_checkpoint(config, checkpoint):
-    global local_rank
-    map_location = {'cuda:0': local_rank}
-    state = torch.load(checkpoint, map_location=map_location)
-    
-    model = SRI(config['K'], config['pixel_bound'], config['z_dim'], 
-                config['semiconv'], config['kernel'], config['pixel_std'],
-                config['dynamic_K'], config['image_likelihood'], False,
-                config['img_size'], config['s_dim'], config['L'])
+def restore_from_checkpoint(args):
 
-    model.load_state_dict(state['model_state_dict'])
-    print(f'loaded {checkpoint}')
-    model = model.to(local_rank)
-    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[local_rank],
-                                                      output_device=local_rank)
+    state = torch.load(args['checkpoint'])
+    
+    if args['model'] == 'SRI':
+        model = SRI(args['K'], args['pixel_bound'], args['z_dim'], 
+                    args['semiconv'], args['kernel'], args['pixel_std'],
+                    args['dynamic_K'], args['image_likelihood'], False,
+                    args['img_size'], args['s_dim'], args['L'])
+    elif args['model'] == 'FixedOrderSRI':
+        model = FixedOrderSRI(args['slot_attention_iters'], args['K'],
+                          args['pixel_bound'], args['z_dim'], 
+                          args['pixel_std'], args['image_likelihood'], 
+                          args['img_size'], args['s_dim'], args['L'])
+    model = model.to(args['device'])
+    new_state_dict = {}
+    for k,v in state['model_state_dict'].items():
+        # remove string 'module.' from the key
+        if 'module.' in k:
+            new_state_dict[k.replace('module.', '')] = v
+        else:
+            new_state_dict[k] = v
+
+    model.load_state_dict(new_state_dict)
+    print(f'loaded {args["checkpoint"]}')
     return model
 
 
@@ -62,11 +72,10 @@ def tensor_to_png(tensor, save_dir, count, stop):
     return count
 
 
-def fid_from_model(model, test_loader, ckpt, results_file=None, batch_size=10,
-                    num_images=10000, feat_dim=2048, img_dir='/tmp', temp=1.0,
-                    verbose=True, model_name='SRI'):
+def fid_from_model(model, test_loader, out_dir, batch_size=10,
+                    num_images=10000, feat_dim=2048, img_dir='/tmp',
+                    verbose=True):
     # Save images from test set as pngs
-    
     test_dir = osp.join(img_dir, 'test_images')
     if not os.path.isdir(test_dir):
         if verbose:
@@ -89,28 +98,26 @@ def fid_from_model(model, test_loader, ckpt, results_file=None, batch_size=10,
     count = 0
     for _ in tqdm(range(num_images // batch_size + 1), disable=not verbose):
         with torch.no_grad():
-            recons, _ = model.module.sample(batch_size=batch_size, temp=temp, K_steps=model.module.K_steps)
+            recons, _ = model.sample(batch_size=batch_size, K_steps=model.K_steps)
 
         count = tensor_to_png(recons, gen_dir, count, num_images)
         if count >= num_images:
             break
     # Compute FID
-    if verbose:
-        print("Computing FID.")
+    print("Computing FID.")
     gpu = next(model.parameters()).is_cuda
     fid_value = FID.calculate_fid_given_paths(
         [test_dir, gen_dir], batch_size, gpu, feat_dim, verbose)
     
     print(f"FID: {fid_value}")
 
-    model.train()
-    if results_file is not None:
-        with open(results_file, 'a+') as f:
-            f.write('{},{}\n'.format(ckpt,fid_value))
+   
+    with open(out_dir / 'fid.txt', 'a+') as f:
+        f.write('{}\n'.format(fid_value))
     return fid_value
 
 
-def fid_from_folder(model_folder, test_loader, results_file, img_dir='/tmp', batch_size=10, num_images=10000,
+def fid_from_folder(model_folder, test_loader, out_dir, img_dir='/tmp', batch_size=10, num_images=10000,
                     feat_dim=2048, verbose=False):
      # Save images from test set as pngs
     
@@ -134,60 +141,51 @@ def fid_from_folder(model_folder, test_loader, results_file, img_dir='/tmp', bat
     
     print(f"FID: {fid_value}")
 
-    if results_file is not None:
-        with open(results_file, 'a+') as f:
-            f.write('{},{}\n'.format(model_folder,fid_value))
+    with open(out_dir / 'fid.txt', 'a+') as f:
+        f.write('{}\n'.format(fid_value))
     return fid_value
 
 
-def run(config, seed):
-    global local_rank
+def main(args):
 
     # Fix random seed
+    seed = args['seed']
     print(f'setting random seed to {seed}')
-    
-    # Auto-set by sacred
+    torch.manual_seed(seed)
     torch.backends.cudnn.deterministic=True
     torch.backends.cudnn.benchmark=False
+    np.random.seed(seed)
 
-    local_rank = 'cuda:{}'.format(local_rank)
-    assert local_rank == 'cuda:0', 'Eval should be run with a single process'
 
-    os.environ['MASTER_ADDR'] = '127.0.0.1'
-    os.environ['MASTER_PORT'] = str(config['DDP_port'])
-    torch.distributed.init_process_group(backend='nccl')
-    torch.cuda.set_device(local_rank)
-
-    te_dataset = ShapeStacksDataset(data_dir=config['data_dir'], mode=config['mode'], load_instances=False)
-    te_dataloader = torch.utils.data.DataLoader(te_dataset, batch_size=config['batch_size'],
-                                                shuffle=True, num_workers=config['num_workers'],
-                                                drop_last=True)
-    checkpoint = pathlib.Path(config['checkpoint_dir'], config['checkpoint'])
+    te_dataset = ShapeStacksDataset(data_dir=args['data_dir'], mode='test', load_instances=False)
+    te_dataloader = torch.utils.data.DataLoader(te_dataset, batch_size=args['batch_size'],
+                                                shuffle=True, num_workers=args['num_workers'],
+                                                drop_last=False)
     
-    if config['from_folder'] == "":
-        model = restore_from_checkpoint(config, checkpoint)
+    out_dir = Path(args['out_dir'], 'results', 
+                   Path(args['checkpoint']).stem + f'-test-seed={seed}')
+    
+    if not out_dir.exists():
+        out_dir.mkdir(parents=True)
+
+    if args['from_folder'] == "":
+        model = restore_from_checkpoint(args)
         model.eval()
         # Compute FID
-        fid_from_model(model, te_dataloader, config['checkpoint'], 
-                    config['results_file'], config['batch_size'],
-                    config['num_fid_images'], config['feat_dim'], config['img_dir'],
-                    1.0, model_name='SRI')
+        fid_from_model(model, te_dataloader, out_dir, args['batch_size'],
+                    args['num_fid_images'], args['feat_dim'], args['img_dir'])
     else:
-        fid_from_folder(config['from_folder'], te_dataloader, config['results_file'],config['img_dir'],
-                        config['batch_size'], config['num_fid_images'], config['feat_dim'], verbose=True)
+        fid_from_folder(args['from_folder'], te_dataloader,out_dir, args['img_dir'],
+                        args['batch_size'], args['num_fid_images'], args['feat_dim'], verbose=True)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser('segregate-reglate-imagine FID')
+    parser = argparse.ArgumentParser('segregate-relate-imagine FID')
 
-    parser.add_argument('--DDP_port', type=int, default=29500,
-                        help='torch.distributed config')
-    parser.add_argument('--local_rank', type=int, default=0,
-                        help='rank of process in the node')
+
     parser.add_argument('--seed', type=int, default=0,
                         help='random seed')
-    parser.add_argument('--num_workers', type=int, default=2,
+    parser.add_argument('--num_workers', type=int, default=4,
                         help='Workers for dataloading')
-    
     parser.add_argument('--from_folder', type=str, default='',
                         help='Provide a folder containing generated images. Used ' \
                              'for computing FID for a model defined external to this repo.')
@@ -199,17 +197,14 @@ if __name__ == '__main__':
                         help='.png directory')
     parser.add_argument('--batch_size', type=int, default=10,
                         help='Generate images in batches')
-    parser.add_argument('--mode', type=str, default='test',
-                        help='Name of dataset split')
     parser.add_argument('--data_dir', type=str, default='',
                         help='Parent folder of data')
-                        
-    parser.add_argument('--checkpoint_dir', type=str, default='./model',
-                        help='The dir containing the provided checkpoint file')
     parser.add_argument('--checkpoint', type=str, default='',
                         help='Trained model weights')
-    parser.add_argument('--results_file', type=str, default='results_FID.txt',
-                        help='File to store FID score in')
+    parser.add_argument('--out_dir', type=str, default='experiments',
+                        help='output folder for results')
+    parser.add_argument('--device', type=str, default='cuda:0',
+                        help='device to run on') 
     
     ### Model
     parser.add_argument('--z_dim', type=int, default=64,
@@ -235,7 +230,9 @@ if __name__ == '__main__':
                         help='Global std dev for image likelihood')
     parser.add_argument('--image_likelihood', type=str, default='MoG-Normed',
                         help='MoG or MoG-Normed (use normalization constant)')
+    parser.add_argument('--slot_attention_iters', type=int, default=3, 
+                        help='number of iterations in slot attention')
                         
     args = vars(parser.parse_args())
 
-    run(args, args['seed'])
+    main(args)
